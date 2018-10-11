606858760a0fd2a4a3d652f81ee68e846f484c5f
Ayush Goel <ayushgoel@goelayu.eecs.umich.edu>
2018-10-11 18:00:14 -0400

    clean up fondue code

--- modifiedFondue/test/scripts/logs-console.log.js
+++
@@ -1,8 +0,0 @@
-function a() { logTest(); infoTest(); warnTest(); errorTest(); traceTest() }
-function logTest() { console.log(1) }
-function infoTest() { console.info(1) }
-function warnTest() { console.warn(1) }
-function errorTest() { console.error(1) }
-function traceTest() { console.trace(1) }
-a();
-setTimeout(function () { a(); }, 100);
