affc1fd11f2177aebf5158778d8537ce88d852c1
ShaghayeghMrdn <shaghayeghmardani@gmail.com>
2020-11-20 13:34:27 -0800

    Fixed scheduler code style

--- horcrux-scheduler/scheduler.js
+++ horcrux-scheduler/scheduler.js
@@ -1,17 +1,17 @@
 // this is the main-thread scheduler
 const __callScheduler__ = function(fnBody, fnSignature) {
-    let fnArgs = '';
+    const fnArgs = '';
     // prepare the input arguments for the fnBody using the signature
     fnSignature.forEach((dependency) => {
-        if (dependency[0].startsWith("closure")) {
-            console.error("Cannot handle:", dependency);
-        }
-        else if (!dependency[0].startsWith("global")) {
+        if (dependency[0].startsWith('closure')) {
+            console.error('Cannot handle:', dependency);
+        } else if (!dependency[0].startsWith('global')) {
             console.log('Besides global and cloure:', dependency);
         }
         // no action is needed, if it's a global depedendency
     });
-    console.log("LOG: fnArgs:'" + fnArgs + "'");
-    let reconstructed = new Function(fnArgs, fnBody);
+    console.log('LOG: fnArgs:"' + fnArgs + '"');
+    const reconstructed = new Function(fnArgs, fnBody);
     reconstructed();
 };
+
