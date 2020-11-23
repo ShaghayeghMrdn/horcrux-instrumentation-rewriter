71215a75bd5dba1322b05a19d985b9567f9250be
ShaghayeghMrdn <shaghayeghmardani@gmail.com>
2020-11-22 21:20:59 -0800

    Sets up a given number of web workers
    - setting up web workers in the begining of callScheduler.
    - Fixed minor dictation errors.
    - The last comment in the file is related to spacemacs spellchecker.

--- horcrux-scheduler/scheduler.js
+++ horcrux-scheduler/scheduler.js
@@ -1,20 +1,24 @@
 const webWorkerScriptID = '__horcrux_worker__';
+const numOfWorkers = 1;
 
 /**
- * defines the __callScheduler__ function globally
- * Initializes the related fields including: list of available workers,
- * Horcrux special event queue, ...
+ * defines the __callScheduler__ function globally which acts as wake up call.
+ * sets up a number of workers, and adds them to the list of available workers.
+ * TODO: initialize Horcrux special event queue
  */
 function __defineScheduler__() {
+    // List of all workers
+    const workers = [];
     // List of available workers
-    this.availableWorker = [];
+    const availableWorkers = [];
+    setUpWorkers();
 
     /** Wakes up the main scheduler to handle:
      * case 1: TODO
      * case 2: when a function wanted to be invoked inside <script>
      * -- This function is defined as a property of window so that
      * it can be called from inside rewritten IIFE and async functions.
-     * @param {string} fnBody stringified function body, sent to constrcutor
+     * @param {string} fnBody stringified function body, sent to constructor
      * @param {Array} fnSignature list of function dependencies
      */
     window.__callScheduler__ = function(fnBody, fnSignature) {
@@ -26,7 +30,7 @@ function __defineScheduler__() {
             } else if (!dependency[0].startsWith('global')) {
                 console.log('Besides global and cloure:', dependency);
             }
-            // no action is needed, if it's a global depedendency
+            // no action is needed, if it's a global dependency
         });
         console.log('LOG: fnArgs:"' + fnArgs + '"');
         const reconstructed = new Function(fnArgs, fnBody);
@@ -40,27 +44,48 @@ function __defineScheduler__() {
     function mainThreadListener(event) {
         console.log(`Main received: ${JSON.stringify(event.data)}`);
         if (event.data.status == 'setup') {
-            const setupTime = event.data.ts - setupStart;
+            // TODO
+            const setupTime = event.data.ts;
             console.log(`worker setup time: ${setupTime}`);
-        } else if (event.data.status == 'executed') {
+        }
+        /*else if (event.data.status == 'executed') {
             const inputTime = event.data.inputReceived - inputStart;
             const runtime = event.data.runtime;
             console.log(`input time: ${inputTime}, runtime: ${runtime}`);
-        }
+        }*/
     }
 
-    const setupStart = Date.now();
-    let inputStart;
-    const blob = new Blob([
-        document.getElementById(webWorkerScriptID).textContent,
-    ], {
-        type: 'text/javascript',
-    });
-    const worker = new Worker(window.URL.createObjectURL(blob));
-    worker.addEventListener('message', mainThreadListener);
+    /**
+     * Sets up numOfWorkers of workers using with the content of <script>
+     * identified by webWorkerScriptID.
+     * @param {int} workerId
+     */
+    function setUpWorkers() {
+        for (let workerId = 0; workerId < numOfWorkers; ++workerId) {
+            const start = Date.now();
+            const blob = new Blob([
+                document.getElementById(webWorkerScriptID).textContent,
+            ], {
+                type: 'text/javascript',
+            });
+            const worker = new Worker(window.URL.createObjectURL(blob));
+            worker.addEventListener('message', mainThreadListener);
+            const workerInfo = {
+                id: workerId,
+                workerObj: worker,
+                setupStart: start,
+                assignedFunction: null,
+                inputStart: null,
+            };
+            workers.push(workerInfo);
+            availableWorkers.push(workerInfo);
+        }
+    };
 };
 
 if (typeof __scheduler__ === 'undefined') {
     __scheduler__ = new __defineScheduler__();
 }
 
+
+//  LocalWords:  workerId workerInfo
