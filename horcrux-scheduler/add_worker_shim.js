dbc38ab1595b5a2985905b1a145acd9d462debb4
ShaghayeghMrdn <shaghayeghmardani@gmail.com>
2021-02-16 10:43:24 -0800

    Added type as second param to add_worker_shim

--- horcrux-scheduler/add_worker_shim.js
+++ horcrux-scheduler/add_worker_shim.js
@@ -2,20 +2,23 @@ const fs = require("fs")
 const program = require('commander');
 
 program
-    .option("-i, --input [input]","HTML/JS file to add the worker shim")
+    .option("-i, --input [input]", "HTML/JS file to add the worker shim")
+    .option("-t, --type [type]", "[html, js]", "html")
     .parse(process.argv);
 
 src = fs.readFileSync(program.input, "utf-8")
-src = `
-<script>
-    let usesWorker = false
-    const __origWorker__ = window.Worker;
-    const myWorker = function(url) {
-        usesWorker = true
-        console.log('>>>>>>>>>> uses web workers!')
-        return new __origWorker__(url)
-    }
-    window.Worker = myWorker;
-</script>` + src;
+if (program.type != "js") {
+    src = `
+    <script>
+        let usesWorker = false
+        const __origWorker__ = window.Worker;
+        const myWorker = function(url) {
+            usesWorker = true
+            console.log('>>>>>>>>>> uses web workers!')
+            return new __origWorker__(url)
+        }
+        window.Worker = myWorker;
+    </script>` + src;
+}
 
 fs.writeFileSync(program.input, src)
